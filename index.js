1d7388f8770b71c066e275a818f7b138614c1f0e
Samuel Loup <loup@epsitec.ch>
2014-12-23 12:06:55 +0100

    initial package impl.

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+/* Xcraft activity manager */
+
+
+var moduleName   = 'activity';
+var busClient    = require ('xcraft-core-busclient');
+var xLog         = require ('xcraft-core-log') (moduleName);
+var FlakeId      = require('flake-idgen');
+var flakeIdGen   = new FlakeId();
+
+exports.create = function (cmd, msg) {
+  flakeIdGen.next(function (err, id) {
+    if (err) {
+      xLog.err (err);
+    }
+    var activity = {
+      id: id,
+      cmd: cmd,
+      msg: msg
+    };
+    busClient.events.send ('activity.started', activity);
+  });
+};
+
+exports.resume = function (id) {
+  // Todo:
+  console.log (id);
+};
+
+exports.destroy = function (id) {
+  // Todo:
+  console.log (id);
+};
