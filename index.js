3a7874f54eeb6dc3feed35e2eef5c63040046960
Samuel Loup <samuel.loup@gmail.com>
2015-04-15 15:16:54 +0200

    Implement activity queuing
    When a command is send, we check if the orcname is different,
    in this case the activity manager queue the next command to run.
    In other case commands are run under the same activity.
    The event log contains now activity.started and activity.finished.

--- index.js
+++ index.js
@@ -5,10 +5,41 @@ var moduleName = 'activity';
 var busClient  = require ('xcraft-core-busclient');
 var xLog       = require ('xcraft-core-log') (moduleName);
 var FlakeId    = require ('flake-idgen');
-
 var flakeIdGen = new FlakeId ();
 
-exports.create = function (cmd, msg) {
+var running    = null;
+var pending    = [];
+
+
+var start = function (activity) {
+  var msg = activity.msg;
+  var cmd = activity.cmd;
+  var action = activity.run;
+
+  running = activity;
+  busClient.events.send ('activity.started', activity);
+
+  var activityEndPromise = new Promise (function (resolve) {
+    var finishTopic = msg.orcName + '::' + cmd + '.finished';
+    busClient.events.subscribe (finishTopic, function () {
+      resolve ();
+    });
+
+    // Effectively run action
+    action (cmd, msg);
+  });
+
+  activityEndPromise.then (function () {
+    busClient.events.send ('activity.finished', activity);
+    running = null;
+    if (pending.length > 0) {
+      var nextActivity = pending.shift ();
+      start (nextActivity);
+    }
+  });
+};
+
+exports.create = function (cmd, msg, action) {
   flakeIdGen.next (function (err, id) {
     if (err) {
       xLog.err (err);
@@ -16,10 +47,14 @@ exports.create = function (cmd, msg) {
     var activity = {
       id: id,
       cmd: cmd,
-      msg: msg
+      msg: msg,
+      run: action
     };
-
-    busClient.events.send ('activity.started', activity);
+    if (running) {
+      pending.push (activity);
+    } else {
+      start (activity);
+    }
   });
 };
 
