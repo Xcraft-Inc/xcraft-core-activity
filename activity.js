a3f0aadb650feba14e1b755b6445b3ea6152f1ed
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-04-25 19:07:01 +0200

    Cosmetic: prettify

--- activity.js
+++ activity.js
@@ -6,7 +6,6 @@ const activity = require ('.');
 
 const cmd = {};
 
-
 cmd.status = function (msg, resp) {
   const status = activity.status ();
   resp.events.send ('activity.status', status);
@@ -22,6 +21,6 @@ exports.xcraftCommands = function () {
   const xUtils = require ('xcraft-core-utils');
   return {
     handlers: cmd,
-    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json'))
+    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json')),
   };
 };
